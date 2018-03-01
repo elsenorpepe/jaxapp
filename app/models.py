ff668818a59b1b4a3256528ffe18081b01d56030
Russ Pepe <russellpepe@gmail.com>
2018-02-28 19:41:28 -0600

    through chap5

--- app/models.py
+++ app/models.py
@@ -1,13 +1,21 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic') # take note here--init relationship here.  not a db field in User.  Define one-to-many in the one side
+    # take note here--init relationship here.  not a db field in User.  Define one-to-many in the one side
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -29,3 +37,7 @@ class Post(db.Model):
 #    id = db.Column(db.Integer, primary_key=True)
 #    date = db.Column(db.DateTime, default=datetime.utcnow)
 #    tips = db.Column(db.Float)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
