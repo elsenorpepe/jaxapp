64be62c20cb70d1d15eda87a32ab799865a27255
Russ Pepe <russellpepe@gmail.com>
2018-03-27 22:20:02 +0000

    through part 7

--- app/forms.py
+++ app/forms.py
@@ -30,3 +30,16 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    # overloaded constructor.  Not totally wrapping my head around it,
+    #  but routes.py will supply as arg current_user.username, which is set
+    # to instance variable and will be checked by validate_username func below
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is non None:
+                raise ValidationError('Please use a different username.')
--- app/routes.py
+++ app/routes.py
@@ -71,7 +71,8 @@ def user(username):
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    # argument to EditProfileForm below refers back to validation in forms.py
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
